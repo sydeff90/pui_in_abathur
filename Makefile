da20eff238ad0226bf3f0cbb404470e1739815ec
sydeff90 <stefan14voicu@gmail.com>
2021-05-19 23:54:59 +0300

    created window

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@
 #OBJ_NAME specifies the name of our exectuable
 OBJ_NAME = ./bin/executable
 CC = g++
-FLAGS = -IC:\SDL\i686-w64-mingw32\include -LC:\SDL\i686-w64-mingw32\lib -w -lmingw32 -lSDL2main -lSDL2
+FLAGS = -IC:\SDL\i686-w64-mingw32\include -LC:\SDL\i686-w64-mingw32\lib -w $(WEIRD_FLAGS) -lmingw32 -lSDL2main -lSDL2
 WEIRD_FLAGS = -Wl,-subsystem,windows
 
 #This is the target that compiles our executable
-all: main
+all: class main
 
-main: class
+main:
 	$(CC) .\code\main.cpp .\lib\class.o $(FLAGS) -o $(OBJ_NAME)
 
 class:
--- code/main.cpp
+++ code/main.cpp
@@ -3,16 +3,42 @@
 #include <SDL2/SDL.h>
 #include "class.h"
 
-int main( int argc, char *argv[] )
+#define WIDTH 600
+#define HEIGHT 800
+
+int main(int argc, char *argv[])
 {
-    if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL World", 0, 50, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    
+    // Check that the window was successfully created
+    if ( NULL == window )
     {
-        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError( ) << std::endl;
+        // In the case that the window could not be made...
+        std::cout << "Could not create window: " << SDL_GetError( ) << std::endl;
+        return 1;
     }
+    
+    SDL_Event windowEvent;
+    
+    while ( true )
+    {
+        if ( SDL_PollEvent( &windowEvent ) )
+        {
+            if ( SDL_QUIT == windowEvent.type )
+            {
+                break;
+            }
+        }
+    }
+    
+    SDL_DestroyWindow( window );
+    SDL_Quit( );
+    
+    return EXIT_SUCCESS;
 
-    Class* c = new Class();
-    c->print_num(10);
-    delete c;
-
-    return 0;
+    // Class* c = new Class();
+    // c->print_num(10);
+    // delete c;
 }
